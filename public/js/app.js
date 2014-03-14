6135bef8d03bb658e92e1b1dc170ee87309183cd
Tong Xiang <tong.xiang2@gmail.com>
2014-03-13 20:40:19 -0400

    implement google maps

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('gothamcoworking', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gothamcoworking.system', 'gothamcoworking.cwspaces']);
+angular.module('gothamcoworking', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gothamcoworking.system', 'gothamcoworking.cwspaces', 'google-maps']);
 
 angular.module('gothamcoworking.system', []);
 angular.module('gothamcoworking.cwspaces', []);
--- public/js/controllers/cwspaces.js
+++ public/js/controllers/cwspaces.js
@@ -3,6 +3,14 @@
 angular.module('gothamcoworking.cwspaces').controller('CwspacesController', ['$scope', '$stateParams', '$location', 'Global', 'Cwspaces', function ($scope, $stateParams, $location, Global, Cwspaces) {
     $scope.global = Global;
 
+    $scope.map = {
+        center: {
+            latitude: 40.7500, 
+            longitude: -73.940
+        }, 
+        zoom: 11
+    };
+    
     $scope.create = function() {
         var cwspace = new Cwspaces({
             title: this.title,
