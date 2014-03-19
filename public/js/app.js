2b43031914ef1a70ad98a0942ee88ba2b06575c4
Tong Xiang <tong.xiang2@gmail.com>
2014-03-19 16:58:22 -0400

    fix lumpy lower grid display

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('gothamcoworking', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gothamcoworking.system', 'gothamcoworking.cwspaces', 'google-maps']);
+angular.module('gothamcoworking', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'gothamcoworking.system', 'gothamcoworking.cwspaces', 'google-maps', 'ngAutocomplete']);
 
 angular.module('gothamcoworking.system', []);
 angular.module('gothamcoworking.cwspaces', []);
